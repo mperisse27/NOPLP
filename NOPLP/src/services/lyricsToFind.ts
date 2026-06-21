eb8fbf1a71e5823e3aba5dc2e78790930280d1dc
Matteo Perisse <matteo.perisse@gmail.com>
2026-06-21 17:57:28 +0200

    feat: added check to already sung lyrics

--- NOPLP/src/services/lyricsToFind.ts
+++ NOPLP/src/services/lyricsToFind.ts
@@ -2,7 +2,7 @@ import type { TimedLyrics } from "../types/gameTypes";
 
 function getMissingLyrics(baseLyrics: TimedLyrics[], missingNumber: number): TimedLyrics[] {
     const numberOfLyrics = baseLyrics.length;
-    const minimum = numberOfLyrics * 0.4; //Play a minimum of the song
+    const minimum = numberOfLyrics * 0.2; //Play a minimum of the song
 
     const missingNumberByPoints = {
         10: 2,
@@ -12,13 +12,35 @@ function getMissingLyrics(baseLyrics: TimedLyrics[], missingNumber: number): Tim
         50: 8
     }
 
+    const repeatingLyrics = baseLyrics
+        .map((lyric, index) => {
+            console.log("Checking lyric:", lyric.lyric, "at index:", index);
+            console.log(baseLyrics.slice(0, index).findIndex(l => l.lyric === lyric.lyric))
+            return baseLyrics.slice(0, index).findIndex(l => l.lyric === lyric.lyric) != -1;
+        });
+
+    let validLyrics = repeatingLyrics.map((isRepeating, index) =>
+        !isRepeating &&
+        index > minimum &&
+        baseLyrics[index].lyric.length > missingNumber
+        ? index : -1
+    ).filter(index => index !== -1);
+
+    console.log("Valid lyrics indices:", validLyrics);
     let missingIndex = -1;
-    while (missingIndex == -1) {
-        const randomIndex = Math.floor(Math.random() * (numberOfLyrics - minimum) + minimum);
+    while (missingIndex == -1 && validLyrics.length > 0) {
+        console.log(validLyrics.length);
+        const randomIndex = validLyrics[Math.floor(Math.random() * validLyrics.length)];
         if (baseLyrics[randomIndex].lyric.length > missingNumber) {
             baseLyrics[randomIndex].missing = missingNumberByPoints[missingNumber as keyof typeof missingNumberByPoints];
             missingIndex = randomIndex;
         }
+        else {
+            validLyrics = validLyrics.filter(index => index !== randomIndex);
+        }
+    }
+    if (validLyrics.length === 0) {
+        console.warn("No valid lyrics found to remove words from.");
     }
     return baseLyrics;
 }
